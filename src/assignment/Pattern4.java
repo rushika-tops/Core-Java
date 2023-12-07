134fed3ce1260c967d9240320d89bc8c8a013bef
Rushika Koshti <137075345+rushika-tops@users.noreply.github.com>
2023-12-07 22:11:10 +0530

    Update Pattern4.java

--- src/assignment/Pattern4.java
+++ src/assignment/Pattern4.java
@@ -1,5 +1,13 @@
 package assignment;
 
 public class Pattern4 {
+  int n=5;
+  for(int i=1;i<=n;i++){
+   for(int j=1;i<=j;j++){
+     System.out.print("*");
+   }
+  System.out.println();
+}
+  
 
 }
